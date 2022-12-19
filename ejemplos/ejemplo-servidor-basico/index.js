a13ec15568948f6833185f5b8e54b543a6669bf3
Sara-95-Sara <sara.galstyan.95@gmail.com>
2022-12-19 15:48:13 +0100

    used chance

--- ejemplos/ejemplo-servidor-basico/index.js
+++ ejemplos/ejemplo-servidor-basico/index.js
@@ -1,10 +1,13 @@
 // cargar libreria http
 const http = require('http');
+const Chance = require('chance');
+
+const chance = new Chance();
 
 // definir un servidor
 const server = http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-type': 'text/plain'});
-    response.end(`Hola soy un servidor HTTP`);
+    response.writeHead(200, { 'Content-type': 'text/html'});
+    response.end(`Hola  ${chance.name()} soy un servidor <b>HTTP</b>`);
 });
 
 // arrancar el servidor
