486bdc43f7c6efbdc624a4ee4e00acde146737ba
Sara-95-Sara <sara.galstyan.95@gmail.com>
2023-01-07 14:13:58 +0100

    Módulos

---
+++ ejemplo-modulos/calculadora.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('inicializo la calculadora');
+
+
+// exports es un objeto vacio.
+module.exports.suma = function(a, b) {
+    return a + b;
+};
+
+module.exports.resta = function(a,b) {
+    return a - b;
+};
---
+++ ejemplo-modulos/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const calculadora = require('./calculadora');
+const reloj = require('./reloj');
+
+console.log(calculadora.suma(2, 5));
+console.log(reloj());
